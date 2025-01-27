1df16d2af358fb335eafd135e699022b43c17adc
Ramya Panditi <ramya.panditi@couchbase.com>
2025-01-27 12:11:09 +0530

    journal

---
+++ frontend/src/hooks/query.type.ts
@@ -0,0 +1,12 @@
+export type jounrnalEntry = {
+    id: string;
+    title: string;
+    description: string;
+  };
+
+export type frontendEntry ={
+  title: string;
+    description: string;
+
+}
+  
